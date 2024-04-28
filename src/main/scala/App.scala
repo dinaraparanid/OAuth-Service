b3c2ab208061f5281562c3cc3beec469ee29ad52
dinaraparanid <dinaraparanid@gmail.com>
2024-04-28 23:09:20 +0300

    routing

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -12,14 +12,24 @@ import org.http4s.{HttpRoutes, Request, Response}
 import org.http4s.ember.server.EmberServerBuilder
 
 object App extends IOApp:
-  private def onTest(name: String): IO[Response[IO]] =
+  private def sendToken(name: String): IO[Response[IO]] =
     Ok(generateToken(name).map(_.getOrElse("")))
 
   private def authBackService: Kleisli[IO, Request[IO], Response[IO]] =
     HttpRoutes
       .of[IO]:
-        case GET → (Root / "test" / name) ⇒ onTest(name)
-        case _                            ⇒ NotFound("Unknown request")
+        case GET → (Root / "oauth" / "authorize")
+          :? ClientIdParamMatcher(clientId)
+          +& RedirectUrlParamMatcher(redirectUrl) ⇒ Ok(f"Redirect to $redirectUrl")
+
+        case GET → (Root / "oauth" / "token")
+          :? GrantTypeParamMatcher(grantType)
+          +& ClientIdParamMatcher(clientId)
+          +& ClientSecretParamMatcher(clientSecret)
+          +& AuthCodeParamMatcher(code)
+          +& RedirectUrlParamMatcher(redirectUrl) ⇒ sendToken(f"$clientId")
+
+        case _ ⇒ NotFound("Unknown request")
       .orNotFound
 
   override def run(args: List[String]): IO[ExitCode] =
---
+++ src/main/scala/QueryParams.scala
@@ -0,0 +1,9 @@
+package com.paranid5.authback
+
+import org.http4s.dsl.io.*
+
+object ClientIdParamMatcher     extends QueryParamDecoderMatcher[Int]("client_id")
+object ClientSecretParamMatcher extends QueryParamDecoderMatcher[String]("client_secret")
+object RedirectUrlParamMatcher  extends QueryParamDecoderMatcher[String]("redirect_url")
+object GrantTypeParamMatcher    extends QueryParamDecoderMatcher[String]("grant_type")
+object AuthCodeParamMatcher     extends QueryParamDecoderMatcher[String]("auth_code")
